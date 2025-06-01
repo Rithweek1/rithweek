d3f96b184c8e6ab51b759ec869c2fd86494150ab
Rithweek1 <rithweekshetty2007@gmail.com>
2025-06-01 14:04:23 +0530

    Add files via upload

---
+++ task1 .py
@@ -0,0 +1,12 @@
+a=input('enter the first number: ')
+a=int(a)
+b=input('enter the second number: ')
+b=int(b)
+c=a + b
+print('Addition: ',c)
+d=a - b
+print('subtraction: ',d)
+e=a * b
+print('multiplication: ',e)
+f=a / b
+print('division: ',f)
